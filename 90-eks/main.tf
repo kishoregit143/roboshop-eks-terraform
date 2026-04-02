fea49614487a57d61afb8ce294bd17289e9179ce
kishore <pagadalamokisthu@gmail.com>
2026-04-02 19:58:33 +0530

    k8s

--- 90-eks/main.tf
+++ 90-eks/main.tf
@@ -3,16 +3,16 @@ module "eks" {
   version = "~> 21.0" # this is module version
 
   name               = local.common_name_suffix
-  kubernetes_version = "1.33"
-#   kubernetes_version = var.eks_version
+  # kubernetes_version = "1.33"
+  # kubernetes_version = var.eks_version
 
   addons = {
     coredns                = {}
     eks-pod-identity-agent = {
       before_compute = true
     }
-    kube-proxy             = {}
-    vpc-cni                = {
+    kube-proxy = {}
+    vpc-cni = {
       before_compute = true
     }
     metrics-server = {}
@@ -24,77 +24,41 @@ module "eks" {
   vpc_id                   = local.vpc_id
   subnet_ids               = local.private_subnet_ids
   control_plane_subnet_ids = local.private_subnet_ids
+
   create_node_security_group = false
   create_security_group      = false
-  node_security_group_id = local.eks_node_sg_id
-  security_group_id = local.eks_control_plane_sg_id
 
-  # EKS Managed Node Group(s)
+  node_security_group_id = local.eks_node_sg_id
+  security_group_id      = local.eks_control_plane_sg_id
+
+  # ✅ Single node group (replaced blue & green)
   eks_managed_node_groups = {
-    blue = {
-      create = var.enable_blue
+    default = {
+      create = true
       ami_type       = "AL2023_x86_64_STANDARD"
-    #   kubernetes_version = var.eks_nodegroup_blue_version
+      # kubernetes_version = var.eks_nodegroup_blue_version  # optional
       instance_types = ["m5.xlarge"]
-      iam_role_additional_policies  = {
+
+      iam_role_additional_policies = {
         amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
         amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
       }
+
+      # cluster nodes autoscaling
+      min_size     = 2
+      max_size     = 10
+      desired_size = 2
+
+      labels = {
+        nodegroup = "default"
+      }
     }
-      
-      # cluster nodes autoscaling
-      min_size     = 2
-      max_size     = 10
-      desired_size = 2
-
-      # taints = {
-      #   upgrade = {
-      #     key = "upgrade"
-      #     value = "true"
-      #     effect = "NO_SCHEDULE"
-      #   }
-      # }
-
-    #   labels = {
-    #     nodegroup = "blue"
-    #   }
-    # }
-
-    # green = {
-    #   create = var.enable_green
-    #   ami_type       = "AL2023_x86_64_STANDARD"
-    #   kubernetes_version = var.eks_nodegroup_green_version
-    #   instance_types = ["m5.xlarge"]
-    #   iam_role_additional_policies  = {
-    #     amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
-    #     amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-    #   }
-      
-      # cluster nodes autoscaling
-      min_size     = 2
-      max_size     = 10
-      desired_size = 2
-
-      # taints = {
-      #   upgrade = {
-      #     key = "upgrade"
-      #     value = "true"
-      #     effect = "NO_SCHEDULE"
-      #   }
-      # }
-
-#       labels = {
-#         nodegroup = "green"
-#       }
-#     }
-#   }
+  }
 
   tags = merge(
     local.common_tags,
     {
-        Name = local.common_name_suffix
+      Name = local.common_name_suffix
     }
   )
-
-    }
-  }
+}
