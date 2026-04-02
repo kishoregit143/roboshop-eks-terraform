4f42e242238a661fe5ffbcb8d65bc0c3c6d1f560
kishore <pagadalamokisthu@gmail.com>
2026-04-02 12:44:57 +0530

    k8s

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 backend "s3" {
     bucket = "devopslab-remote-state"
-    key    = "ec2-eks-eksctlvpc"
+    key    = "ec2-eks-demo"
     region = "us-east-1"
     use_lockfile = true
     encrypt = true
