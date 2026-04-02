0e7fc93705c3d09b79a844b97cad5404413d1324
kishore <pagadalamokisthu@gmail.com>
2026-04-02 12:40:52 +0530

    k8s

--- 10-sg/provider.tf
+++ 10-sg/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 backend "s3" {
     bucket = "devopslab-remote-state"
-    key    = "ec2-eks-sg"
+    key    = "ec2-eks-eksctlsg"
     region = "us-east-1"
     use_lockfile = true
     encrypt = true
