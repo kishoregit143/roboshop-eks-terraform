5efb1298e9bf57d5c13abbc9b288941a842c9c7b
kishore <pagadalamokisthu@gmail.com>
2026-04-02 19:41:50 +0530

    k8s

---
+++ 90-eks/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.31.0"
+    }
+  }
+backend "s3" {
+    bucket = "devopslab-remote-state"
+    key    = "ec2-eks-eks"
+    region = "us-east-1"
+    use_lockfile = true
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
---
+++ 90-eks/variables.tf
@@ -0,0 +1,36 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z03838132KMLUODN1AMHO"
+}
+
+variable "domain_name" {
+    default = "devopslab.fun
+"
+}
+
+variable "eks_version" {
+
+}
+
+variable "eks_nodegroup_blue_version" {
+
+}
+
+variable "eks_nodegroup_green_version" {
+
+}
+
+variable enable_blue {
+
+}
+
+variable enable_green {
+    
+}
