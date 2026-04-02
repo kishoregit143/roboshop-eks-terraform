552255845fca2fa4b32e38e2d7b472ffd75d95cb
kishore <pagadalamokisthu@gmail.com>
2026-04-02 14:22:52 +0530

    k8s

---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z03838132KMLUODN1AMHOs"
+}
+
+variable "domain_name" {
+    default = "devopslab.fun"
+}
