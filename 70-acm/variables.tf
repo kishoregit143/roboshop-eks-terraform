5153bd4fe8f47f9d7f29220cb036eeaea64cc4c1
kishore <pagadalamokisthu@gmail.com>
2026-04-02 14:11:54 +0530

    k8s

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "devopslab.fun"
+}
+
+variable "zone_id" {
+    default = "Z03838132KMLUODN1AMHO"
+}
