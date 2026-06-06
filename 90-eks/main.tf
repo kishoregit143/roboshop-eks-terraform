97628604768b468cba5b3d195054a3639a3cfc7d
kishore <pagadalamokisthu@gmail.com>
2026-06-06 08:33:56 +0530

    ansible

--- 90-eks/main.tf
+++ 90-eks/main.tf
@@ -1,10 +1,11 @@
 module "eks" {
+
+  #it give instructions to aws to create cluser and nodes
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0" # this is module version
 
   name               = local.common_name_suffix
-  kubernetes_version = "1.32"
-  # kubernetes_version = var.eks_version
+  kubernetes_version = "1.33"
 
   addons = {
     coredns                = {}
@@ -32,9 +33,7 @@ module "eks" {
   # EKS Managed Node Group(s)
   eks_managed_node_groups = {
     blue = {
-      # create = var.enable_blue
       ami_type       = "AL2023_x86_64_STANDARD"
-      # kubernetes_version = var.eks_nodegroup_blue_version
       instance_types = ["m5.xlarge"]
       iam_role_additional_policies  = {
         amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
@@ -45,7 +44,24 @@ module "eks" {
       min_size     = 2
       max_size     = 10
       desired_size = 2
-   
+
+    }
+     
+    green = {
+      
+      ami_type       = "AL2023_x86_64_STANDARD"
+      
+      instance_types = ["m5.xlarge"]
+      iam_role_additional_policies  = {
+        amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+        amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+      }
+      
+      # cluster nodes autoscaling
+      min_size     = 2
+      max_size     = 10
+      desired_size = 2
+
       # taints = {
       #   upgrade = {
       #     key = "upgrade"
@@ -53,42 +69,9 @@ module "eks" {
       #     effect = "NO_SCHEDULE"
       #   }
       # }
-
-      # labels = {
-      #   nodegroup = "blue"
-      # }
-    }
-      #till Blue it is node grop creating we have eanable green for cluster upgradation
-    # green = {
-    #   create = var.enable_green
-    #   ami_type       = "AL2023_x86_64_STANDARD"
-    #   kubernetes_version = var.eks_nodegroup_green_version
-    #   instance_types = ["m5.xlarge"]
-    #   iam_role_additional_policies  = {
-    #     amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
-    #     amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-    #   }
-      
-    #   # cluster nodes autoscaling
-    #   min_size     = 2
-    #   max_size     = 10
-    #   desired_size = 2
-
-    #   taints = {
-    #     upgrade = {
-    #       key = "upgrade"
-    #       value = "true"
-    #       effect = "NO_SCHEDULE"
-    #     }
-    #   }
-
-    #   # labels = {
-    #   #   nodegroup = "green"
-    #   # }
-    # }
+      } 
   }
-
-  tags = merge(
+    tags = merge(
     local.common_tags,
     {
         Name = local.common_name_suffix
@@ -96,3 +79,4 @@ module "eks" {
   )
   
 }
+
