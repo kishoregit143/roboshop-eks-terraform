5b0a296ebd5cd0b1bdb347e690480f2746c0e184
kishore <pagadalamokisthu@gmail.com>
2026-06-06 09:55:21 +0530

    ansible

--- 90-eks/main.tf
+++ 90-eks/main.tf
@@ -1,11 +1,10 @@
 module "eks" {
-
-  #it give instructions to aws to create cluser and nodes
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0" # this is module version
 
   name               = local.common_name_suffix
-  kubernetes_version = "1.33"
+  # kubernetes_version = "1.33"
+  kubernetes_version = var.eks_version
 
   addons = {
     coredns                = {}
@@ -33,24 +32,9 @@ module "eks" {
   # EKS Managed Node Group(s)
   eks_managed_node_groups = {
     blue = {
+      create = var.enable_blue
       ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["m5.xlarge"]
-      iam_role_additional_policies  = {
-        amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
-        amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-      }
-      
-      # cluster nodes autoscaling
-      min_size     = 2
-      max_size     = 10
-      desired_size = 2
-
-    }
-     
-    green = {
-      
-      ami_type       = "AL2023_x86_64_STANDARD"
-      
+      kubernetes_version = var.eks_nodegroup_blue_version
       instance_types = ["m5.xlarge"]
       iam_role_additional_policies  = {
         amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
@@ -69,9 +53,42 @@ module "eks" {
       #     effect = "NO_SCHEDULE"
       #   }
       # }
-      } 
+
+      labels = {
+        nodegroup = "blue"
+      }
+    }
+
+    green = {
+      create = var.enable_green
+      ami_type       = "AL2023_x86_64_STANDARD"
+      kubernetes_version = var.eks_nodegroup_green_version
+      instance_types = ["m5.xlarge"]
+      iam_role_additional_policies  = {
+        amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+        amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+      }
+      
+      # cluster nodes autoscaling
+      min_size     = 2
+      max_size     = 10
+      desired_size = 2
+
+      # taints = {
+      #   upgrade = {
+      #     key = "upgrade"
+      #     value = "true"
+      #     effect = "NO_SCHEDULE"
+      #   }
+      # }
+
+      labels = {
+        nodegroup = "green"
+      }
+    }
   }
-    tags = merge(
+
+  tags = merge(
     local.common_tags,
     {
         Name = local.common_name_suffix
@@ -79,4 +96,3 @@ module "eks" {
   )
   
 }
-
